c92bca21bc80cf8ce592e7aa55f0c2ffbff3959d
RomanOt <vatokato@gmail.com>
2020-06-20 19:40:58 +0500

    build

---
+++ build/precache-manifest.bd20f335ed2aec5d37f10e224b9f2ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3afea319eee6bb335e2ead740ffbe59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1e5a60f6fee81761cb3",
+    "url": "./static/css/main.42876393.chunk.css"
+  },
+  {
+    "revision": "9aa450a5d42936d0d690",
+    "url": "./static/js/2.d2300860.chunk.js"
+  },
+  {
+    "revision": "e1e5a60f6fee81761cb3",
+    "url": "./static/js/main.7166bf2e.chunk.js"
+  },
+  {
+    "revision": "a690f1fc5026b2dbeb8a",
+    "url": "./static/js/runtime-main.a084fcc1.js"
+  },
+  {
+    "revision": "d61c3928e813d263f14e82710466ed3e",
+    "url": "./static/media/logo.d61c3928.svg"
+  }
+]);
