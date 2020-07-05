d726da55a5d07e70942ef820cf01125a2ff6ae93
Roman Otstavnov <vatokato@gmail.com>
2020-07-05 20:27:41 +0500

    build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = (env, argv) => ({
   entry: [
     '@babel/polyfill',
     path.resolve(__dirname, "src", "index.js"),
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.(sa|sc)ss$/,
         use: [
-          process.env.NODE_ENV === 'production'
+          argv.mode === 'production'
             ? MiniCssExtractPlugin.loader
             : 'style-loader',
           {
@@ -30,24 +30,10 @@ module.exports = {
               modules: {
                 localIdentName: '[name]__[local]',
               },
-              sourceMap: true,
+              sourceMap: argv.mode !== 'production',
             },
           },
           'sass-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          process.env.NODE_ENV === 'production'
-            ? MiniCssExtractPlugin.loader
-            : 'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
         ],
       },
       {
@@ -84,25 +70,22 @@ module.exports = {
       chunkFilename: '[id].css',
     })
   ],
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: 'src',
     historyApiFallback: true,
     port: 3000,
     proxy: {
       '/api/': {
-        target: 'http://134.0.119.35:5000',
-        // target: 'http://localhost:5000',
+        target: `http://${argv.server}`,
         pathRewrite: { '/api/': '' },
         secure: false,
         changeOrigin: true,
       },
       '/socket.io/': {
-        target: 'http://134.0.119.35:5000',
-        // target: 'http://localhost:5000',
+        target: `http://${argv.server}`,
         secure: false,
         ws: true,
       },
     }
   }
-};
+});
